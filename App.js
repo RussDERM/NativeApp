93d89a90e5a9ed23a39c1f64a3ad4a9a053467cc
Russell Dermody <russ.dermody@gmail.com>
2020-10-21 13:54:17 -0700

    Color Box Exercise

--- App.js
+++ App.js
@@ -4,27 +4,39 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View
-        style={[
-          styles.container,
-          styles.limegreen,
-          // { backgroundColor: 'pink' },
-        ]}
-      >
-        <Text>Hello, World!</Text>
+      <View style={[styles.container]}>
+        <Text style={[styles.colorBox, { color: 'black' }]}>Color Boxes</Text>
+        <Text style={[styles.Cyan, styles.colorBox]}>Cyan: #2aa198</Text>
+        <Text style={[styles.colorBox, { backgroundColor: '#268bd2' }]}>
+          Blue: #268bd2
+        </Text>
+        <Text style={[styles.colorBox, { backgroundColor: '#d33682' }]}>
+          Magenta: #d33682
+        </Text>
+        <Text style={[styles.colorBox, { backgroundColor: '#cb4b16' }]}>
+          Orange: #cb4b16
+        </Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  limegreen: {
-    backgroundColor: 'limegreen',
+  colorBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    paddingVertical: 10,
+    color: 'white',
+  },
+  Cyan: {
+    backgroundColor: 'cyan',
   },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 10,
   },
 });
 
